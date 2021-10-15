6c417792bb3b39102d69f9e244ab647c3ee8944a
xayom1996 <xayom1996@gmail.com>
2021-10-15 16:12:13 +0300

    commit 15.10

--- android/app/src/main/kotlin/com/radio/barbaros/MainActivity.kt
+++ android/app/src/main/kotlin/com/radio/barbaros/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.radiobarbaros
+package com.radio.barbaros;
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
@@ -6,14 +6,9 @@ import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 // Added these three import lines
-import android.view.WindowManager;
-import android.view.WindowManager.LayoutParams;
-import android.os.Bundle;
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
-        // Added this line
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
     }
 }
